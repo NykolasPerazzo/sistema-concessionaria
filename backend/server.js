a72c916bc7cc4adb0fbda3483fb202010ea38266
Nykolas <nykolasrg@icloud.com>
2026-08-23 09:15:55 -0300

    feat: adiciona dotenv ao servidor

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Car Dealer IA API funcionando!"
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
